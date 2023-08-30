0c703473d63c5c5795cd874eb95e49c22a9ac34c
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-30 16:01:05 -0600

    Added an automatic update check

--- index.js
+++ index.js
@@ -6,10 +6,13 @@
  *
  * @author Reece Poulsen <https://www.yansalabs.com/>
  */
+const { spawn } = require('child_process');
 const chalk = require('chalk');
+const ora = require('ora');
 const { init } = require('./utils/init.js');
 const { cli } = require('./utils/cli.js');
 const { log } = require('./utils/log.js');
+const { printHeader } = require('./utils/utilities.js');
 
 // Import command functions
 const { yalaSetup } = require('./commands/yalaSetup.js');
@@ -20,6 +23,57 @@ const { yalaDeleteComponent } = require('./commands/yalaDeleteComponent.js');
 const { yalaAddAction } = require('./commands/yalaAddAction.js');
 const { yalaAddProperty } = require('./commands/yalaAddProperty.js');
 const { yalaCreateXml } = require('./commands/yalaCreateXml.js');
+
+/**
+ * This function checks if yala-component-cli needs updated and runs the update if needed
+ */
+const checkForUpdates = async () => {
+	printHeader('Checking for updates');
+	const updateSpinner = ora(
+		'Checking for yala-component-cli updates'
+	).start();
+
+	// run npm view yala-component-cli to check the latest version
+	const npmViewCommand = new Promise((resolve, reject) => {
+		const npmView = spawn('npm', ['view', 'yala-component-cli', 'version']);
+		npmView.stdout.on('data', output => {
+			resolve(output.toString());
+		});
+	});
+	const latestVersion = await npmViewCommand;
+
+	// run yala -v to see what is installed
+	const yalaVersionCommand = new Promise((resolve, reject) => {
+		const yalaVersion = spawn('yala', ['-v']);
+		yalaVersion.stdout.on('data', output => {
+			resolve(output.toString());
+		});
+	});
+	const curVersion = await yalaVersionCommand;
+
+	if (curVersion != latestVersion) {
+		// If an update is needed, run npm update yala-component-cli -g
+		updateSpinner.warn(
+			chalk.yellow(
+				'A new version of yala-component-cli is available, updating now'
+			)
+		);
+		const updateCommand = new Promise((resolve, reject) => {
+			const npmUpdate = spawn(
+				'npm',
+				['update', 'yala-component-cli', '-g'],
+				{ stdio: [0, 0, 0] }
+			);
+			npmUpdate.on('close', code => resolve('ok'));
+		});
+		await updateCommand;
+		updateSpinner.succeed(chalk.green('yala-component-cli updated'));
+	} else {
+		updateSpinner.succeed(
+			chalk.green('Running latest yala-component-cli version')
+		);
+	}
+};
 
 /**
  * Main function for the cli, used to route commands
@@ -35,10 +89,13 @@ const { yalaCreateXml } = require('./commands/yalaCreateXml.js');
 		log(flags);
 		log(input);
 	}
-	if (input.includes(`help`) || flags.help) {
+	if (input.includes(`help`) || flags.help || input.length == 0) {
 		cli.showHelp(0);
 		return;
 	}
+
+	// Check for updates
+	await checkForUpdates();
 
 	// Handle commands
 	if (input[0] == 'setup') {
