ef6f0450918a14d1998fe582741961ea3d68d256
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-30 15:20:23 -0600

    increment version num

--- utils/init.js
+++ utils/init.js
@@ -8,7 +8,7 @@ const init = ({ clear = true }) => {
 		tagLine: `by Yansa Labs`,
 		description:
 			"Yansa Labs' improved CLI solution for ServiceNow custom component development",
-		version: '1.0.5',
+		version: '1.0.6',
 		bgColor: '#36BB09',
 		color: '#000000',
 		bold: true,
