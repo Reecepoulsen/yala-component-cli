ab1b74e6809b579ee6e699587b0bb2ee628fd97b
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-30 15:18:40 -0600

    removed reading package.json

--- utils/init.js
+++ utils/init.js
@@ -1,16 +1,14 @@
 const welcome = require('cli-welcome');
 const unhandled = require('cli-handle-unhandled');
-const fs = require('fs');
-
-const packageJson = JSON.parse(fs.readFileSync('./package.json'));
 
 const init = ({ clear = true }) => {
 	unhandled();
 	welcome({
 		title: `yala-component-cli`,
 		tagLine: `by Yansa Labs`,
-		description: packageJson.description,
-		version: packageJson.version,
+		description:
+			"Yansa Labs' improved CLI solution for ServiceNow custom component development",
+		version: '1.0.5',
 		bgColor: '#36BB09',
 		color: '#000000',
 		bold: true,
