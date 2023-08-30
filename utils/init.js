c65157322d2005b54b91c7f80be6e8537600ce6a
Reece Poulsen <dreecepoulsen@gmail.com>
2023-08-30 10:29:58 -0600

    updated tagline and version number

--- utils/init.js
+++ utils/init.js
@@ -5,10 +5,10 @@ const init = ({ clear = true }) => {
 	unhandled();
 	welcome({
 		title: `yala-component-cli`,
-		tagLine: `by Reece Poulsen`,
+		tagLine: `by Yansa Labs`,
 		description:
 			"Yansa Labs' improved CLI solution for ServiceNow custom component development",
-		version: '1.0.0',
+		version: '1.0.3',
 		bgColor: '#36BB09',
 		color: '#000000',
 		bold: true,
